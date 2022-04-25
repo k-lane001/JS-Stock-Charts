fe351694b448fa6a7642a4a85b0f9fd44b1b183f
Kaylie Lane <k.lane5612@gmail.com>
2022-04-25 13:13:40 -0400

    First Chart Working

--- public/index.js
+++ public/index.js
@@ -4,6 +4,41 @@ async function main() {
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
+    let response = await fetch('https://api.twelvedata.com/time_series?apikey=5c57b35fdfeb4889b9c923bdee1ffa78&interval=1month&symbol=GME,MSFT,DIS,BNTX');
+
+    let result = await response.json();
+    console.log(result);
+    const {GME, MSFT, DIS, BNTX} = result;
+    const stocks = [GME, MSFT, DIS, BNTX];
+
+    function getColor(stock){
+        if(stock === "GME"){
+            return 'rgba(61, 161, 61, 0.7)'
+        }
+        if(stock === "MSFT"){
+            return 'rgba(209, 4, 25, 0.7)'
+        }
+        if(stock === "DIS"){
+            return 'rgba(18, 4, 209, 0.7)'
+        }
+        if(stock === "BNTX"){
+            return 'rgba(166, 43, 158, 0.7)'
+        }
+    }
+    stocks.forEach(stock => stock.values.reverse())
+//Time Chart
+    var myChart = new Chart(timeChartCanvas.getContext('2d'), {
+        type: 'line',
+        data: {
+            labels: stocks[0].values.map(value => value.datetime),
+            datasets: stocks.map(stock => ({
+                label: stock.meta.symbol,
+                data: stock.values.map(value => parseFloat(value.high)),
+                backgroundColor: getColor(stock.meta.symbol),
+                borderColor: getColor(stock.meta.symbol),
+            }))
+        }
+    });
 }
 
 main()

f507da7e049a1ed66de6e936821ac9eeb7e68b98
Kaylie Lane <k.lane5612@gmail.com>
2022-04-25 17:28:20 -0400

    Working on High Chart and forEach Loop

--- public/index.js
+++ public/index.js
@@ -3,14 +3,17 @@ async function main() {
     const timeChartCanvas = document.querySelector('#time-chart');
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
-
-    let response = await fetch('https://api.twelvedata.com/time_series?apikey=5c57b35fdfeb4889b9c923bdee1ffa78&interval=1month&symbol=GME,MSFT,DIS,BNTX');
+//Fetch data from twelvedata.com
+    let response = await fetch('https://api.twelvedata.com/time_series?apikey=5c57b35fdfeb4889b9c923bdee1ffa78&interval=1day&symbol=GME,MSFT,DIS,BNTX');
 
     let result = await response.json();
     console.log(result);
+
+
     const {GME, MSFT, DIS, BNTX} = result;
     const stocks = [GME, MSFT, DIS, BNTX];
 
+    //Color function for 
     function getColor(stock){
         if(stock === "GME"){
             return 'rgba(61, 161, 61, 0.7)'
@@ -25,8 +28,10 @@ async function main() {
             return 'rgba(166, 43, 158, 0.7)'
         }
     }
-    stocks.forEach(stock => stock.values.reverse())
-//Time Chart
+
+    stocks.forEach(stock => stock.values.reverse());
+
+    //Time Chart
     var myChart = new Chart(timeChartCanvas.getContext('2d'), {
         type: 'line',
         data: {
@@ -39,6 +44,30 @@ async function main() {
             }))
         }
     });
+
+    //         //Loop over Array for High Chart
+    //         let temp = 0
+    //         stocks.forEach((values) => {
+    //             if(temp < values) {
+    //                 temp = values;
+    //             }
+    //         }
+    //         );
+
+    // //High Chart
+    // var highPriceChart = new Chart(highestPriceChartCanvas.getContext('2d'), {
+    //     type: 'bar',
+    //     data: {
+    //     labels: stocks[0],
+    //     datasets: `${temp}`,
+    //     backgroundColor: getColor(stock.meta.symbol),
+    //     borderColor: getColor(stock.meta.symbol),
+    //     },
+    
+    //     }
+    // );
+
+
 }
 
 main()
